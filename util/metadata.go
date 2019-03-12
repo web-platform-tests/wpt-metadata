d9308bcf75ca516177e0d10fc6a9020c3a9bfbe1
Kyle Ju <kyleju@chromium.org>
2019-03-12 17:41:11 -0400

    nit

--- util/metadata.go
+++ util/metadata.go
@@ -2,42 +2,41 @@ package util
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"	
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
-func CheckError(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
 // CollectMetadata iterates through wpt-metadata repository and returns a
 // map that maps a test path to its META.yml file content.
-func CollectMetadata() map[string][]byte{
+func CollectMetadata() map[string][]byte {
 	ymlFiles := []string{}
 	curPath, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
 
-	rootDor := filepath.Dir(filepath.Dir(curPath))
-	walkErr := filepath.Walk(rootDor, func(path string, f os.FileInfo, err error) error {
+	rootDir := filepath.Dir(filepath.Dir(curPath))
+	walkErr := filepath.Walk(rootDir, func(path string, f os.FileInfo, err error) error {
 		if f.Name() == "META.yml" {
 			ymlFiles = append(ymlFiles, path)
-		}	
-        return err
+		}
+		return err
 	})
-	CheckError(walkErr)
+	if walkErr != nil {
+		fmt.Println(walkErr)
+	}
 
 	ymlMap := make(map[string][]byte)
 	for _, file := range ymlFiles {
 		data, err := ioutil.ReadFile(file)
-		CheckError(err)
-		testPath := strings.TrimPrefix(file, rootDor)
-		ymlMap[testPath] = data 
+		if err != nil {
+			fmt.Println(err)
+		}
+		testPath := strings.TrimPrefix(file, rootDir)
+		ymlMap[testPath] = data
 	}
+
 	return ymlMap
 }
