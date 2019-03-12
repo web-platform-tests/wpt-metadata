097b15f1e46b6b933c2ca37afa7a69d232324ec5
Kyle Ju <kyleju@chromium.org>
2019-03-12 16:55:56 -0400

    Create a function that collects all metadata for wpt.fyi

---
+++ util/metadata.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"	
+	"io/ioutil"
+	"strings"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+// CollectMetadata iterates through wpt-metadata repository and returns a
+// map that maps a test path to its META.yml file content.
+func CollectMetadata() map[string][]byte{
+	ymlFiles := []string{}
+	curPath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	rootDor := filepath.Dir(filepath.Dir(curPath))
+	walkErr := filepath.Walk(rootDor, func(path string, f os.FileInfo, err error) error {
+		if f.Name() == "META.yml" {
+			ymlFiles = append(ymlFiles, path)
+		}	
+        return err
+	})
+	CheckError(walkErr)
+
+	ymlMap := make(map[string][]byte)
+	for _, file := range ymlFiles {
+		data, err := ioutil.ReadFile(file)
+		CheckError(err)
+		testPath := strings.TrimPrefix(file, rootDor)
+		ymlMap[testPath] = data 
+	}
+	return ymlMap
+}
+
+func main() {
+	fmt.Println(CollectMetadata())
+}
