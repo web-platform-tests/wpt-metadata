2cfef46944a094b1c15b95fbc9f53d46cf1978fc
Robert Ma <bob1211@gmail.com>
2019-03-24 19:23:39 -0400

    Apply suggestions from code review
    Remove a white line
    
    Co-Authored-By: KyleJu <kyleju@google.com>

--- util/metadata.go
+++ util/metadata.go
@@ -16,7 +16,6 @@ const url = "https://api.github.com/repos/web-platform-tests/wpt-metadata/tarbal
 // CollectMetadata iterates through wpt-metadata repository and returns a
 // map that maps a test path to its META.yml file content.
 func CollectMetadata() (res map[string][]byte, err error) {
-
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
