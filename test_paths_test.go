5bff7c76c02c95634eccf670d3c991ca57a55177
Kyle Ju <kyleju@google.com>
2020-07-11 15:34:25 -0400

    Close file descriptor (#282)
    Co-authored-by: Kyle Ju <kyleju@chromium.org>

--- test_paths_test.go
+++ test_paths_test.go
@@ -64,6 +64,8 @@ func TestResultsTestPaths(t *testing.T) {
 				if err != nil {
 					panic(err)
 				}
+				defer f.Close()
+
 				data, err := ioutil.ReadAll(f)
 				if err != nil {
 					panic(err)
